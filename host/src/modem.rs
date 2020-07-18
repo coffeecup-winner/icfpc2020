53dfb5bbe1da18460e9f87e1592c23adafadcd90
Victor "multun" Collod <victor.collod@epita.fr>
2020-07-17 21:17:12 -0700

    fix modulate interface, add demodulate

--- host/src/modem.rs
+++ host/src/modem.rs
@@ -1,6 +1,6 @@
-pub enum List {
+pub enum NestedList {
     Number(i64),
-    List(Vec<List>),
+    List(Vec<NestedList>),
 }
 
 pub fn mod_num(number: i64) -> Vec<bool> {
@@ -9,7 +9,7 @@ pub fn mod_num(number: i64) -> Vec<bool> {
     res
 }
 
-pub fn mod_list(list: &List) -> Vec<bool> {
+pub fn mod_list(list: &NestedList) -> Vec<bool> {
     panic!()
 }
 
@@ -17,7 +17,7 @@ pub fn dem_num(data: &Vec<bool>) -> i64 {
     panic!()
 }
 
-pub fn dem_list(data: &Vec<bool>) -> List {
+pub fn dem_list(data: &Vec<bool>) -> NestedList {
     panic!()
 }
 
@@ -54,4 +54,30 @@ fn modulate(signed_num: i64, res: &mut Vec<bool>) -> () {
         let mask = 1u64 << encoded_space - 1 - i;
         res.push(num & mask == 0);
     }
+}
+
+fn demodulate(iter: &mut Iterator<Item=bool>) -> Result<i64,()> {
+    let negative = match iter.next() { Some(val) => val, None => return Err(()) };
+    match iter.next() { Some(val) => val, None => return Err(()) };
+
+    let used_nibbles = 'outer: loop {
+        for i in 0u64.. {
+            if ! match iter.next() { Some(val) => val, None => return Err(()) } {
+                break 'outer i;
+            }
+        }
+    };
+
+    let mut res = 0u64;
+    for i in 0..(used_nibbles * 4) {
+        if match iter.next() { Some(val) => val, None => return Err(()) } {
+            res |= 1u64 << i;
+        }
+    }
+
+    Ok(if negative {
+        -(res as i64)
+    } else {
+        res as i64
+    })
 }
