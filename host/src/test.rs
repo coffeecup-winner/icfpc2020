4e2c7ac5d694054c18962de5ed5c065cbaff403a
Victor "multun" Collod <victor.collod@epita.fr>
2020-07-18 20:38:32 -0700

    implement cons function / pair duality

--- host/src/test.rs
+++ host/src/test.rs
@@ -11,12 +11,19 @@ struct State {
 enum Value {
     Lambda(NamedLambda),
     Number(usize),
+    Nil,
+    True,
+    False,
+    Cons(Rc<Value>, Rc<Value>),
+    Apply(Rc<Value>, Rc<Value>),
 }
 
 impl Value {
     fn call(&self, arg: Rc<Value>) -> Result<Rc<Value>, ()> {
         match self {
             Value::Lambda(named_lambda) => Ok((named_lambda.lambda)(arg)),
+            // callins a cons is like applying something to the pair
+            Value::Cons(a, b) => Ok(Value::apply(Value::apply(arg, a.clone()), b.clone())),
             _ => Err(()),
         }
     }
@@ -34,6 +41,14 @@ impl Value {
     fn number(number: usize) -> Rc<Value> {
         return Rc::new(Value::Number(number));
     }
+
+    fn cons(a: Rc<Value>, b: Rc<Value>) -> Rc<Value> {
+        return Rc::new(Value::Cons(a, b));
+    }
+
+    fn apply(f: Rc<Value>, x: Rc<Value>) -> Rc<Value> {
+        return Rc::new(Value::Apply(f, x));
+    }
 }
 
 type LambdaBox = Box<dyn Fn(Rc<Value>) -> Rc<Value>>;
@@ -50,25 +65,29 @@ impl fmt::Debug for NamedLambda {
     }
 }
 
-fn test_bltin() -> Rc<Value> {
-    Value::lambda("test", move |x| {
-        Value::lambda("test'", move |y| {
-            let x = match *x {
-                Value::Number(n) => n,
-                _ => panic!(""),
-            };
-            let y = match *y {
-                Value::Number(n) => n,
-                _ => panic!(""),
-            };
-            Value::number(x + y)
-        })
+fn builtin_cons() -> Rc<Value> {
+    Value::lambda("cons", move |a| {
+        Value::lambda("cons'", move |b| Value::cons(a.clone(), b.clone()))
     })
 }
 
-fn main() -> Result<(), ()>{
+fn builtin_true() -> Rc<Value> {
+    Value::lambda("true", move |a| {
+        Value::lambda("true'", move |_b| a.clone())
+    })
+}
+
+fn builtin_false() -> Rc<Value> {
+    Value::lambda("false", move |_a| {
+        Value::lambda("false'", move |b| b.clone())
+    })
+}
+
+fn main() -> Result<(), ()> {
     let mut vars = HashMap::new();
-    vars.insert(String::from("test"), test_bltin());
+    vars.insert(String::from("cons"), builtin_cons());
+    vars.insert(String::from("t"), builtin_true());
+    vars.insert(String::from("f"), builtin_false());
 
     let a = Value::number(12);
     let b = Value::number(30);
@@ -76,7 +95,10 @@ fn main() -> Result<(), ()>{
     let state = State { vars };
 
     let result_func = state.vars["test"].clone();
-    println!("result: {:?}", result_func.call(a.clone())?.call(b.clone())?);
+    println!(
+        "result: {:?}",
+        result_func.call(a.clone())?.call(b.clone())?
+    );
 
     Ok(())
 }
